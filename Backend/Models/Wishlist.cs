41d7f833704441987c2c20cc14f39a5ca1fb68d1
Githubjael <74548587+Githubjael@users.noreply.github.com>
2026-03-11 06:51:49 +0100

    .gitignore->Debug add, Wishlist start

---
+++ Backend/Models/Wishlist.cs
@@ -0,0 +1,8 @@
+public class Wishlist
+{
+    public int Id{get;set;}
+    public string Name{get;set;}
+    public List<Product> Products;
+    public int Total{get;set;}
+    public Wishlist(){}
+}
