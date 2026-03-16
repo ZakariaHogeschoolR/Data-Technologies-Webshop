653ef29a7fa44de050ecd2aa13dfaa304006a078
Githubjael <74548587+Githubjael@users.noreply.github.com>
2026-03-16 08:38:37 +0100

    Implement NotImplementedException for WishlistService methods

--- Backend/WebshopApi/Services/WishlistService.cs
+++ Backend/WebshopApi/Services/WishlistService.cs
@@ -8,10 +8,22 @@ namespace Service
         {
             _wishlistRepository = wishlistRepository;
         }
-        public async Task<List<Wishlists?>> GetAllWishLists(){}
-        public async Task<Wishlists?> GetWishlistsById(){}
-        public async Task<List<Products?>> GetAllProducts(){}
-        public async Task<Products?> GetProductsById(){}
+        public async Task<List<Wishlists?>> GetAllWishLists()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<Wishlists?> GetWishlistsById()
+        {
+            throw new NotImplementedException();
+        }   
+        public async Task<List<Products?>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<Products?> GetProductsById()
+        {
+            throw new NotImplementedException();
+        }
         public async void CreateService(){}
         public async void UpdateService(){}
         public async void DeleteService(){}
