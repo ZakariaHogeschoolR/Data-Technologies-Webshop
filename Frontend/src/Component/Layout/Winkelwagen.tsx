458cd98f725d57edbe4ee4a245c3710204dbae4d
Githubjael <74548587+Githubjael@users.noreply.github.com>
2026-03-22 22:45:00 +0100

    add Winkelwagen component with item list and total price display

---
+++ Frontend/src/Component/Layout/Winkelwagen.tsx
@@ -0,0 +1,17 @@
+export default function Winkelwagen(){
+    return(
+        <>
+        <div className="Winkelwagen_container">
+            <div className="items-lijst">
+                <ul>
+                    <li>Poduct1 name, price: 11.99, quantity: 2</li>
+                    <li>Poduct2 name, price: 5.99, quantity: 1</li>
+                    <li>Poduct3 name, price: 0.99, quantity: 4</li>
+                    <li>Poduct4 name, price: 59.99, quantity: 2</li>
+                </ul>
+            </div>
+            <div className="total-price">78, 96</div>
+        </div>
+        </>
+    );
+}
