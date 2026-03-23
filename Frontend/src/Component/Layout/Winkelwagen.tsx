038750a57953e40035289758de9522d520083e9a
Githubjael <74548587+Githubjael@users.noreply.github.com>
2026-03-23 10:24:28 +0100

    implement data fetching for Winkelwagen and Wishlist components; handle loading and error states

--- Frontend/src/Component/Layout/Winkelwagen.tsx
+++ Frontend/src/Component/Layout/Winkelwagen.tsx
@@ -1,16 +1,49 @@
+import { useEffect, useState } from 'react';
 import '../../Styles/Winkelwagen.css';
+import { useParams } from 'react-router-dom';
 
 export default function Winkelwagen(){
+    const {id} = useParams()
+    const [winkelwagenItems, setWinkelwagenItems] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(``)
+
+    useEffect(() =>
+        {
+            async function getData(){
+                setLoading(true)
+                try{
+                    const reponse = await fetch(`http://localhost:5261/api/ShoppingCartController/${id}`, {
+                        headers:{
+                            "Content-Type": "application/json",
+                            "Accept": "application/json"
+                        }
+                    })
+                    const json = await reponse.json()
+                    setWinkelwagenItems(json) 
+                }
+                catch(e){
+                    setError(`fetch winkelwagen failed`)
+                }
+                finally{
+                    setLoading(false)
+                }
+            }
+            getData()
+        }, [])
+    if(loading) return <>Loading...</>
+    if(error != null) return <>Error: {error}</>
     return(
         <>
         <div className="Winkelwagen_container">
             <div className="items-lijst">
-                <ul>
+                {winkelwagenItems}
+                {/* <ul>
                     <li>Poduct1 name, price: 11.99, quantity: 2</li>
                     <li>Poduct2 name, price: 5.99, quantity: 1</li>
                     <li>Poduct3 name, price: 0.99, quantity: 4</li>
                     <li>Poduct4 name, price: 59.99, quantity: 2</li>
-                </ul>
+                </ul> */}
             </div>
             <div className="total-price">153.91</div>
         </div>
