a8829b0bbf16aeef4bd385129b9481b62da4b3ef
Githubjael <74548587+Githubjael@users.noreply.github.com>
2026-04-29 20:16:22 +0200

    winkelwagen delete works

--- Frontend/src/Component/Layout/Winkelwagen.tsx
+++ Frontend/src/Component/Layout/Winkelwagen.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import '../../Styles/Winkelwagen.css';
 
 interface winkelwagen{
@@ -26,8 +26,61 @@ export default function Winkelwagen(){
     const [inError, setInError] = useState(false)
 
     const token = localStorage.getItem(`token`)
-    const DeleteProductFromWinkelwagen = async(productId:number) => {
+
+    const fetchData = useCallback(async () => {
+        if(!token){
+            setLoading(false)
+            return;
+        }
+        try{
+            setLoading(true)
+            const request = await fetch(`http://localhost:5261/api/ShoppingCart/mine`, {
+                headers:{
+                    "Authorization":`Bearer ${token}`
+                }
+            })
+            // console.log(request)
+            const json : winkelwagen[] = await request.json()
+            // console.log(json)
+            setWinkelwagenItems(json)
+
+            if(json.length > 0){
+                const productPromises = json.map( (ww) =>
+                    fetch(`http://localhost:5261/api/Product/${ww.productId}`).then(r=> r.json())
+                )
+
+                const results = await Promise.all(productPromises) as product[]
+                setProducts(results)
+            }
+        } catch(err){
+            setInError(true)
+            setError(`${err}`)
+            console.log(`error: `+ err)
+        } finally{
+            setLoading(false)
+        }
+    }, [token])
+    useEffect(() =>{
+        fetchData()
+    }, [fetchData])
+
+    // gebruik useMemo() voor het caching van een resultaat
+    // van een calculatie tussen rerenders(pagina herladen). cool!
+    // https://react.dev/reference/react/useMemo
+    const totalPrice = useMemo(()=>{
+        return winkelwagenItems.reduce((total, item) => {
+            const product = products.find(p => p.id === item.productId)
+            return total + (product ? product.price * item.quantity : 0)
+        }, 0)
+    }, [winkelwagenItems, products])
+
+    const DeleteProductFromWinkelwagen = async(e:React.MouseEvent, productId:number) => {
+        e.preventDefault()
+
+        console.log(`Delete gestart op product: `, productId)//log1
+
         const url = `http://localhost:5261/api/ShoppingCart/delete/product`
+
         try{
             const response = await fetch(url, {
                 method: `DELETE`,
@@ -36,10 +89,14 @@ export default function Winkelwagen(){
                     "Accept": "application/json",
                     "Authorization": `Bearer ${token}`
                 },
-                body: JSON.stringify({productId: productId})
+                body: JSON.stringify({id: 0, productId: productId })
+                // backend is supposed to replace
+                // id with one from claims in backend
             })
-            if(response.status == 204){
-                setWinkelwagenItems(previtems => previtems.filter(item => item.productId !== productId))
+            console.log(`Response status:`, response.status)//log2
+            if(response.status === 204){
+                console.log(`Delete succes, data ophalen`)//log3
+                await fetchData()
             }
             else{
                 console.error(`verwijderen mislukt` + response.statusText)
@@ -49,86 +106,22 @@ export default function Winkelwagen(){
             console.log(`netwerk error: `, e)
         }
     }
-    // gebruik useMemo() voor het caching van een resultaat
-    // van een calculatie tussen rerenders(pagina herladen). cool!
-    // https://react.dev/reference/react/useMemo
-    const totalPrice = useMemo(()=>{
-        return winkelwagenItems.reduce((total, item) => {
-            const product = products.find(p => p.id == item.productId)
-            return total + (product ? product.price * item.quantity : 0)
-        }, 0)
-    }, [winkelwagenItems, products])
-    useEffect(() =>
-        {
-            if(!token){
-                setLoading(false)
-                return;
-            }
-            async function getWinkelwagenData(){
-                try{
-                    setLoading(true)
-                    const request = await fetch(`http://localhost:5261/api/ShoppingCart/mine`, {
-                        headers:{
-                            "Content-Type": "application/json",
-                            "Accept": "application/json",
-                            "Authorization":`Bearer ${token}`
-                        }
-                    })
-                    // console.log(request)
-                    const json = await request.json()
-                    // console.log(json)
-                    setWinkelwagenItems(json)
-                }
-                catch(err){
-                    setInError(true)
-                    setError(`${err}`)
-                    console.log(`error: `+ err)
-                }
-                finally{
-                    setLoading(false)
-                }
-            }
-            getWinkelwagenData()
-        }, [token])
-    useEffect(()=>{
-        async function getProductnamesFromWinkelwagen() {
-            if (winkelwagenItems.length === 0) return;
-            try{
-                const productPromises = winkelwagenItems.map(async (ww) =>{
-                    const request = await fetch(`http://localhost:5261/api/Product/${ww.productId}`)
 
-                    return await request.json() as product
-                })
-                const results = await Promise.all(productPromises)
-                setProducts(results)
-            }
-            catch(err){
-                setInError(true)
-                setError(`${err}`)
-                console.log(`error: `+ err)
-            }
-            finally{
-                setLoading(false)
-            }
-        }
-        getProductnamesFromWinkelwagen()
-    }, [winkelwagenItems])
     if(loading) return <>Loading...</>
-    // if (winkelwagenItems.length == 0) return <>No winkelwagens</>
     else if(inError) return <>{error}</>
     else if(!token){
         return(<div className='Winkelwagen_container'>
             <p>Je moet een account hebben en ingelogd zijn om te kunnen shoppen.</p>
         </div>)
     }
-    else if (winkelwagenItems.length == 0) return (<p>Your shoppingcart is empty. Add Products to see them here.</p>)
+    else if (winkelwagenItems.length === 0) return (<p>Your shoppingcart is empty. Add Products to see them here.</p>)
     return(
         <>
         <div className="Winkelwagen_container">
             <div className="items-lijst">
                 <ul>
                     {winkelwagenItems.map((winkelwagen) => {
-                        const product = products.find(p => p.id == winkelwagen.productId)
+                        const product = products.find(p => p.id === winkelwagen.productId)
                         return(
                         <li key={winkelwagen.id}>
                             {product&& (
@@ -138,7 +131,7 @@ export default function Winkelwagen(){
                                 )}
                                 <p>Name: {product ? product.name : `Error...`}; Quantity: {winkelwagen.quantity}; price: {product ? product.price: 0.00};</p>
                                 <p>Sum Price:{product ? (product.price * winkelwagen.quantity).toFixed(2) : '0.00'}</p>
-                                <button onClick={() => DeleteProductFromWinkelwagen(winkelwagen.productId)}>Delete</button>
+                                <button onClick={(e) => DeleteProductFromWinkelwagen(e, winkelwagen.productId)}>Delete</button>
                                 </li>
                                 );
                             }
