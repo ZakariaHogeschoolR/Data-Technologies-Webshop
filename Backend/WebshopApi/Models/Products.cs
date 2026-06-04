9df726ca6dbfe815fb09cff75d8f8f4914acf6ad
mtsahinhr <1065791@hr.nl>
2026-06-04 13:21:08 +0200

    Update Products.cs

--- Backend/WebshopApi/Models/Products.cs
+++ Backend/WebshopApi/Models/Products.cs
@@ -7,5 +7,5 @@ public class Products
     public string Name { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
-    public int TeamId { get; set; }
+    public int? TeamId { get; set; }
 }
