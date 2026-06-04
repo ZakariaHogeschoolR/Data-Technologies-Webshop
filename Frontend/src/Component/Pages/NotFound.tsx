53e62952f253dcaf90c0a909ecb09b44b1272cdb
mtsahinhr <1065791@hr.nl>
2026-06-05 00:40:23 +0200

    Update NotFound.tsx

--- Frontend/src/Component/Pages/NotFound.tsx
+++ Frontend/src/Component/Pages/NotFound.tsx
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
 import '../../Styles/NotFound.css';
-const NotFound = () => 
-{
+
+const NotFound = () => {
     return (
-        <>
-            <div className="NotFound-container">
-                <p>404 NotFound</p>
-            </div>
-        </>
+        <div className="NotFound-container">
+            <p className="NotFound-code">404</p>
+            <p className="NotFound-message">Pagina niet gevonden</p>
+            <Link to="/" className="NotFound-link">← Terug naar home</Link>
+        </div>
     );
-}
+};
+
 export default NotFound;
